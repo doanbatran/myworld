31faa4a5e202a180ad4042807b9f2b84a446093a
Tran Doan <doanbatran69@gmail.com>
2025-08-02 23:16:37 +0700

    Hoàn thành bài học Django Admin và Create User

--- my_tennis_club/members/urls.py
+++ my_tennis_club/members/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
+from django.contrib import admin
 
 urlpatterns = [
     path('', views.main, name='main'),
     path('members/', views.members, name='members'),
     path('members/details/<int:id>', views.details, name='details'),
     path('testing/', views.testing, name='testing'),
+    path('admin/', admin.site.urls),
 ]
