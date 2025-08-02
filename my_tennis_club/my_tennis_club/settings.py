5d109d9e5edecb053417a30db9425a36fbd399ad
Tran Doan <doanbatran69@gmail.com>
2025-08-02 22:43:38 +0700

    Hoàn thành bài học 404 Template

--- my_tennis_club/my_tennis_club/settings.py
+++ my_tennis_club/my_tennis_club/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h^&nq2(_*pa^70!^&9vobofr2ac$7dv-kkqssjggcu97%$f3r^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
