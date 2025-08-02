192ee425e76fd5fcb9b6aa74806cbbf281eb7be5
Tran Doan <doanbatran69@gmail.com>
2025-08-02 10:43:57 +0700

    Hoàn thành bài học Template

--- my_tennis_club/members/views.py
+++ my_tennis_club/members/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
 def members_view(request):
-    return HttpResponse("Hello world!")
+    template = loader.get_template('first.html')
+    return HttpResponse(template.render())
